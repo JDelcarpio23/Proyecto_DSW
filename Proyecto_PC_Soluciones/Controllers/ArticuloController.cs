1e4cdcf83a315ae40892375d2593b21256234b38
Jhamil Saul Rengifo Roiter <i202013753@cibertec.edu.pe>
2022-06-26 01:51:32 -0500

    Version 2

---
+++ Proyecto_PC_Soluciones/Controllers/ArticuloController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering; //selectList
+using Microsoft.Data.SqlClient;
+using System.Data;
+using Proyecto_PC_Soluciones.Models;
+namespace Proyecto_PC_Soluciones.Controllers
+{
+    public class ArticuloController : Controller
+    {
+        string cadena = @"server=DESKTOP-V44JFQH\SQLEXPRESS; database=ProyectoVisual; " +
+        "Trusted_Connection=True; MultipleActiveResultSets=True; TrustServerCertificate=False; Encrypt=False";
+        IEnumerable<Articulo> articulos()
+        {
+            List<Articulo> temporal = new List<Articulo>();
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                SqlCommand cmd = new SqlCommand("exec usp_Articulo", cn);
+                cn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    Articulo obj = new Articulo()
+                    {
+                        id_articulo = dr.GetInt32(0),
+                        nom_articulo = dr.GetString(1),
+                        pre_articulo = dr.GetDecimal(2),
+                        stock_articulo = dr.GetInt32(3),
+                    };
+                    temporal.Add(obj);
+                }
+            }
+            return temporal;
+        }
+        Articulo Buscar(int codigo)
+        {
+            return articulos().Where(c => c.id_articulo == codigo).FirstOrDefault();
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await Task.Run(() => articulos()));
+        }
+        public async Task<IActionResult> Create()
+        {
+            return View(await Task.Run(() => new Articulo()));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Articulo reg)
+        {
+            string mensaje = "";
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("usp_agregar_articulo", cn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@nom_articulo", reg.nom_articulo);
+                    cmd.Parameters.AddWithValue("@pre_articulo", reg.pre_articulo);
+                    cmd.Parameters.AddWithValue("@stock_articulo", reg.stock_articulo);
+                    cn.Open();
+                    int c = cmd.ExecuteNonQuery(); //ejecuta y retorna el numero de reg insertados
+                    mensaje = $"Se ha insertado {c} Articulo";
+                }
+                catch (Exception ex) { mensaje = ex.Message;/*en caso de error*/ }
+                finally { cn.Close(); }
+            }
+            //refrescar la vista, el retorno del POST
+            ViewBag.mensaje = mensaje;
+            return View(await Task.Run(() => reg));
+        }
+        public async Task<IActionResult> Edit(int codigo)
+        {
+            // si no esta vacio id
+            Articulo reg = Buscar(codigo);
+
+            //enviar el Cliente
+            return View(await Task.Run(() => reg));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Articulo reg)
+        {
+            string mensaje = "";
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("usp_actualiza_articulo", cn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@id_articulo", reg.id_articulo);
+                    cmd.Parameters.AddWithValue("@nom_articulo", reg.nom_articulo);
+                    cmd.Parameters.AddWithValue("@pre_articulo", reg.pre_articulo);
+                    cmd.Parameters.AddWithValue("@stock_articulo", reg.stock_articulo);
+                    cn.Open();
+                    int c = cmd.ExecuteNonQuery(); //ejecuta
+                    mensaje = $"Se ha actualizado {c} Articulo";
+                }
+                catch (Exception ex) { mensaje = ex.Message;/*en caso de error*/ }
+                finally { cn.Close(); }
+            }
+            //refrescar la vista, el retorno del POST
+            ViewBag.mensaje = mensaje;
+            return View(await Task.Run(() => reg));
+        }
+
+
+    }
+}
